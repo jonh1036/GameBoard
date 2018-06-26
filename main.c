8c9a412e863602bbac1fd3af9858a08ece1bc7e6
jonh1036 <jonh1036@hotmail.com>
2018-06-26 13:04:45 -0300

    Matrix inicialization

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
+void flush_in();
 
 int main(void) {
-  char mat[4][4];
-  int i, j;
-
-  printf("\n");
-  mat[1][1] = '*';
-  //while(1){
-	for(i = 0; i < 4; i++ ){
-		printf("|");
-		printf("\n");
-	    for(j = 0; j < 4; j++){
+	char mat[4][4], op;
+	int i, j, opT;
+	
+	while(1){//Inicialização da Matriz
+		for(i = 0; i < 4; i++ ){
+			printf("|");
+			
+			if(i == 0){
+				mat[0][0] = '*';
+				printf("%c",mat[0][0]);
+				for(j = 1; j < 4; j++){
+					mat[i][j] = ' ';
+		  			printf("%c",mat[i][j]);
+		   		}	
+			}
+			else{
+				for(j = 0; j < 4; j++){
+					mat[i][j] = ' ';
+		  			printf("%c",mat[i][j]);
+		   		}
+			}
+			printf("|\n");
+		}
+		getch(op);
+		opT = (int) op;
+		flush_in();//Limpando o buffer do teclado
 		
-		mat[i][j] = '*';
-	  	printf("%c",mat[i][j]);
-	   }
-	}    
-  //}
-  printf("\n\n");
-  return 0;
+		switch(opT){
+			case 113: break;
+			default: printf("\nTeclas permitidas: \nSeta direcional esquerda \nSeta direcional superior\nSeta direcional direita\nSeta direcional inferior\n 'Q' para sair");
+					 break;
+		}
+		system("@cls||clear");//Limpando o terminal
+	}
+
+	printf("\n\n");
+	return 0;
+}
+
+void flush_in() {
+	int ch;
+	do {
+		ch = fgetc(stdin);
+	} while (ch != EOF && ch != '\n');
 }
