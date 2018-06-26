2c7fe3be1d8cc3bc1f70702db58f1fe8c1ce9c39
jonh1036 <jonh1036@hotmail.com>
2018-06-26 09:54:33 -0300

    Atribuindo matriz

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
-//teste commit
-
 #include <stdio.h>
 
 int main(void) {
-  int mat[4][4];
+  char mat[4][4];
+  int i, j;
 
-  while(1){
-    
-  }
+  printf("\n");
+  mat[1][1] = '*';
+  //while(1){
+	for(i = 0; i < 4; i++ ){
+	   for(j = 0; j < 4; j++){
+		mat[i][j] = '*';
+	  	printf("%c",mat[i][j]);
+	   }
+	}    
+  //}
+  printf("\n\n");
   return 0;
 }
