c9eb13c43199c9eef8fe9a054d425605179077cb
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-06-26 10:13:03 -0300

    Teste pipe

--- main.c
+++ main.c
@@ -8,7 +8,10 @@ int main(void) {
   mat[1][1] = '*';
   //while(1){
 	for(i = 0; i < 4; i++ ){
-	   for(j = 0; j < 4; j++){
+		printf("|");
+		printf("\n");
+	    for(j = 0; j < 4; j++){
+		
 		mat[i][j] = '*';
 	  	printf("%c",mat[i][j]);
 	   }
